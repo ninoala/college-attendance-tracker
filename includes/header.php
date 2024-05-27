783eb4e4ede15aaa9fc1570a152c1b3ddf90841a
ninoala <yegor.nino@gmail.com>
2024-05-27 11:45:57 -0400

    fixed favicons

--- includes/header.php
+++ includes/header.php
@@ -11,10 +11,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Fraser River College | Attendance Tracker Login</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="ynino.dev/college-tracker/assets/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="ynino.dev/college-tracker/assets/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="ynino.dev/college-tracker/assets/favicons/favicon-16x16.png">
-        <link rel="manifest" href="ynino.dev/college-tracker/assets/favicons/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="/tracker/assets/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/tracker/assets/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/tracker/assets/favicons/favicon-16x16.png">
+        <link rel="manifest" href="ynino.dev/tracker/assets/favicons/site.webmanifest">
         <link rel="stylesheet" href="https://ynino.dev/tracker/css/style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -23,7 +23,7 @@
     <body>
         <header class='header'>
             <div class="header__logo-box">
-                <a href="http://ynino.dev/college-tracker/index.php" class="header__link"><img src="http://ynino.dev/college-tracker/assets/logo.png" alt="Website Logo" class="header__img"></a>
+                <a href="http://ynino.dev/tracker/index.php" class="header__link"><img src="http://ynino.dev/tracker/assets/logo.png" alt="Website Logo" class="header__img"></a>
             </div>
 
             <div class='header__time-box'>
